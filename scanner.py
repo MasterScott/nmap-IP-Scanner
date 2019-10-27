aca630cc51048cb0c55125a9cc5e68d2558b1ede
0n1cOn3 <0n1cOn3@gmx.ch>
2019-10-27 18:52:11 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -7,4 +7,4 @@ elif sys.argv[1].lower()=='scan':
 	f=open(sys.argv[2],'r').readlines()
 	for i in range(len(f)):
 		print('Scanning '+str(i+1)+' out of '+str(len(f))+' IPs')
-		os.system('nmap -sS '+f[i]+' >>nmap-results.txt')
+		os.system('sudo -sS nmap -sS '+f[i]+' >>nmap-results.txt')
